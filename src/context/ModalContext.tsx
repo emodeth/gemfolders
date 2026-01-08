9ae34991dde575da3fe9ec178eb18171da22e77f
Emirhan Keskin <emodethistaken@gmail.com>
2026-01-09 01:40:19 +0300

    ColorPicker type changed to colorPicker

--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, type ReactNode } from 'react';
 
-export type ModalType = 'createFolder' | 'renameFolder' | 'deleteFolder' | 'ColorPicker';
+export type ModalType = 'createFolder' | 'renameFolder' | 'deleteFolder' | 'colorPicker';
 
 interface ModalContextType {
   type: ModalType;
